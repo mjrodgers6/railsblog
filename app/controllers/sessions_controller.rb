36dae6e296144330d8e2319b4211c125b4b73eb1
James Middlemiss <middlemissj.usa@gmail.com>
2015-07-07 14:18:59 -0400

    added signin/signout functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+  def create
+    username = params[:username]
+    password = params[:password]
+
+    @user = User.where(username: username).first
+
+    if @user.nil?
+      #wrong username case
+      flash[:alert] = "Incorrect Credentials"
+      redirect_to root_path
+    else #success username case
+      if @user.password == password
+        session[:user_id] = @user.id
+        flash[:alert] = "Welcome Cheese noodle"
+        redirect_to root_path
+      else  
+        flash[:alert] = "Incorrect Credentials"
+        redirect_to root_path
+      end
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:alert] = "Successful Logout"
+    redirect_to root_path
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  
+  root 'users#index'
   resources :users
+
+  resources :sessions
+
+  delete '/logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
