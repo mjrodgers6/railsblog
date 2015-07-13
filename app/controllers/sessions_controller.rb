74c8b4f325967c29902845f546c4dc3276ca522d
Jamie Rodgers <mjrodgers6@gmail.com>
2015-07-12 20:11:44 -0400

    Serious styling on this git

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,15 +9,15 @@ class SessionsController < ApplicationController
     if @user.nil?
       #wrong username case
       flash[:alert] = "Incorrect Credentials"
-      redirect_to root_path
+      redirect_to posts_path
     else #success username case
       if @user.password == password
         session[:user_id] = @user.id
         flash[:alert] = "Welcome Cheese noodle"
-        redirect_to root_path
+        redirect_to posts_path
       else  
         flash[:alert] = "Incorrect Credentials"
-        redirect_to root_path
+        redirect_to posts_path
       end
     end
   end
